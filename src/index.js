75d1d1e55757a8c29c83a0cf51fffbe37e87200e
Yuri Zakharov <yuri.zaharov@gmail.com>
2023-03-26 18:30:32 +0200

    Fixed application port. Fixed deployment

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(metrics);
 const router = require('./router');
 app.use(router);
 
-app.listen(8088, (err) => {
+app.listen(8080, (err) => {
     if (err) logger.error(err);
     logger.info('Server has been started');
 });
